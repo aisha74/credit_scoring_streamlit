7306d905cf67615de235911e2c9c07480dcd6c3b
aisha74 <kh.aisha74@mail.ru>
2025-03-02 07:42:36 +0000

    Credit_scoring_streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈Credit_scoring_streamlit")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
